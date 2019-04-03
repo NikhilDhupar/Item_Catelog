08ec1ee63938c83d6634580a8091fe882768c357
NikhilDhupar <nikhildhupar207@gmail.com>
2019-04-04 01:07:28 +0530

    Code for Declaring Tables

---
+++ database_setup.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=True)
+
+
+class Book(Base):
+    __tablename__ = 'book'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    description = Column(String(250))
+    author = Column(String(100), nullable=False)
+    price = Column(String(8), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///bookstore.db')
+Base.metadata.create_all(engine)
